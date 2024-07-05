e99786a33bfa3cd37e10ed8d5a9cbd5c689dc14b
oxenprogrammer <emmy1000okello@gmail.com>
2024-07-05 23:18:11 +0300

    refactor:  Update config file

--- config.js
+++ config.js
@@ -1,36 +1,29 @@
 "use strict";
 
-/** Shared config for application; can be required many places. */
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
 
-require("dotenv").config();
-require("colors");
+const myEnv = dotenv.config();
+dotenvExpand.expand(myEnv);
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const config = {
+  SECRET_KEY: process.env.SECRET_KEY || "secret-dev",
+  PORT: parseInt(process.env.PORT, 10) || 3001,
+  BCRYPT_WORK_FACTOR: process.env.NODE_ENV === "test" ? 1 : parseInt(process.env.BCRYPT_WORK_FACTOR, 10) || 12,
 
-const PORT = +process.env.PORT || 3001;
+  getDatabaseUri: () => {
+    return process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL;
+  }
+};
 
-// Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "postgresql:///jobly2_test"
-      : process.env.DATABASE_URL || "postgresql:///jobly2";
-}
-
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
-const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
+// Log configuration
 console.log("Jobly Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
+console.log("SECRET_KEY:".yellow, config.SECRET_KEY);
+console.log("PORT:".yellow, config.PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, config.BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, config.getDatabaseUri());
 console.log("---");
 
-module.exports = {
-  SECRET_KEY,
-  PORT,
-  BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
-};
+module.exports = config;
